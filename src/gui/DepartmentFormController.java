f0abff3eccd0dd89c575e8b6c36956a685489e54
engenheiro.muniz <engenheiro.muniz@gmail.com>
2022-08-11 12:19:23 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,19 +1,27 @@
 package gui;
 
+import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable{
 	
 	private Department entity;
+	
+	private DepartmentService service;
 	
 	@FXML
 	private TextField txtId;
@@ -34,14 +42,46 @@ public class DepartmentFormController implements Initializable{
 		this.entity = entity;
 	}
 	
-	@FXML
-	public void onBtnSaveAction() {
-		System.out.println("The action was saved.");
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
+//	@FXML
+	public void onBtnSaveAction(javafx.event.ActionEvent event) {
+		
+
+		if(entity == null) {
+			throw new IllegalStateException("Entity department was passed null to the class.");
+		}
+		
+		if(service == null) {
+			throw new IllegalStateException("Service was passed null to the class.");
+		}		
+		
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			
+			Utils.currentStage(event).close();
+		
+			
+		}catch (DbException e) {
+			 Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}	
+	}
+	
+	private Department getFormData() {
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		
+		return obj;
+	}
+
 	@FXML
-	public void onBtnCancelAction() {
-		System.out.println("The action was canceled.");
+	public void onBtnCancelAction(javafx.event.ActionEvent event) {
+		Utils.currentStage(event).close();
 	}	
 	
 	@Override
